37618c365662e1ff7354cc16a643dbe5d3185b96
Oleg <gafrom@gmail.com>
2017-10-27 21:12:33 +0300

    Finish on sitemap parsing

--- app/services/charutti/catalog.rb
+++ app/services/charutti/catalog.rb
@@ -20,30 +20,24 @@ module Charutti
       print "Updating links from #{supplier.host}... "
       CSV.open path_to_links_file, 'wb' do |file|
         links = extract_links_from open("#{supplier.host}#{SITEMAP_URL}").read
-        links.each { |url| file << [url] }
+        links.each { |url, last_modified_at| file << [url, last_modified_at] }
       end
       puts 'Done'
     end
 
-    # def extract_links_from(content)
-    #   Nokogiri::XML(content).css('.katalog-image>a.ajax-link').map do |a_node|
-    #     a_node.attr('href')
-    #   end.compact
-    # end
-
-
     def extract_links_from(content)
-      pattern = /\A\/(news\/.*|content\/.*|catalog\/.*|\/)\Z/
+      pattern = /\A\/(news\/.*|content\/.*|catalog\/.*|new-sale\/.*|sale\/.*)?\Z/
       Nokogiri::XML(content).css('url').inject({}) do |links, node|
         url = node.css('loc').first.text.split(supplier.host).last
         modified_at = node.css('lastmod').first.text.to_date
         if url && modified_at
-          links.merge url => modified_at unless url =~ pattern
+          next links if url =~ pattern
+          links.merge url => modified_at
         else
           log_failure_for url, '[SITEMAP PARSING]'
           links
         end
-      end.compact
+      end
     end
 
   end
