c3186265530118e8dc8d45e0f25c5cf34c78133b
Oleg <gafrom@gmail.com>
2017-11-15 21:22:13 +0300

    Add CSV export in batches

--- app/services/export.rb
+++ app/services/export.rb
@@ -2,13 +2,44 @@ require 'csv'
 
 class Export
   CATEGORIES_DEPTH = 5
-  PATH_TO_FILE = Rails.root.join('storage', 'export.csv')
+  PATH_TO_FILE = Rails.root.join('storage', 'export')
+
+  def initialize
+    @files = []
+  end
 
   def csv(how_many = nil)
-    CSV.open PATH_TO_FILE, 'wb' do |file|
-      Product.available.includes(:supplier, :category).limit(how_many).each do |product|
-        product.to_csv { |row| file << row }
-      end
+    CSV.open "#{PATH_TO_FILE}.csv", 'wb' do |file|
+      push Product.available.includes(:supplier, :category).limit(how_many), file
     end
+
+    result
+  end
+
+  def csv_in_batches(batch_size = nil)
+    batch_size ||= 7000
+
+    Product.available
+           .includes(:supplier, :category)
+           .in_batches(of: batch_size)
+           .each_with_index do |batch, num|
+      CSV.open("#{PATH_TO_FILE}_batch_#{num + 1}.csv", 'wb') { |file| push batch, file }
+    end
+
+    result
+  end
+
+  private
+
+  def push(batch, file)
+    batch.each do |product|
+      product.to_csv { |row| file << row }
+    end
+
+    @files << file.path
+  end
+
+  def result
+    @files.size == 1 ? @files.first : @files
   end
 end
--- lib/tasks/export/csv.rake
+++ lib/tasks/export/csv.rake
@@ -3,7 +3,18 @@ namespace :export do
     ARGV.each { |a| task a.to_sym do ; end }
 
     limit = ARGV[1] ? ARGV[1].to_i : nil
-    Export.new.csv limit
-    puts 'CSV file is updated successfully.'
+    file = Export.new.csv limit
+    puts "CSV file `#{file}` is composed successfully ✅"
+  end
+  namespace :csv do
+    task batch: :environment do
+      ARGV.each { |a| task a.to_sym do ; end }
+
+      batch_size = ARGV[1] ? ARGV[1].to_i : nil
+      files = Export.new.csv_in_batches batch_size
+
+      files.map! { |file| "#{file} ✅" }
+      puts "The following CSV files are composed successfully:\n#{files.join("\n")}"
+    end
   end
 end
