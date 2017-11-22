efeb935bc2ca3eba92528eb05fca9a0509e14bb6
Oleg <gafrom@gmail.com>
2017-11-22 23:37:42 +0300

    Add last product detailed info to dashboard

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,16 @@
 class WelcomeController < ApplicationController
   def home
     @suppliers = Supplier.all.map do |supplier|
+      host = Product::PROXY_HOST if Rails.env.development?
+      products = supplier.products.order(updated_at: :desc)
+      last_product = products.first
       {
         name: supplier.name,
-        updated_at: supplier.products.order(updated_at: :desc).first.updated_at,
-        total: supplier.products.count,
-        available: supplier.products.available.count,
-        no_color: supplier.products.available.where(color: nil).count
+        rel_image: "#{host}/#{supplier.slug}#{last_product.images.first}",
+        last_product: last_product,
+        total: products.count,
+        available: products.available.count,
+        no_color: products.available.where(color: nil).count
       }
     end
   end
