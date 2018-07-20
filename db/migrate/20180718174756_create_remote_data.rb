f281192696b7c3997eee0df5e681ae9d2e432f79
Oleg <gafrom@gmail.com>
2018-07-20 16:10:18 +0300

    Revert creation of remote data table

--- db/migrate/20180718174756_create_remote_data.rb
+++
@@ -1,9 +0,0 @@
-class CreateRemoteData < ActiveRecord::Migration[5.1]
-  def change
-    create_table :remote_data do |t|
-      t.integer :remote_id, index: true
-
-      t.timestamps
-    end
-  end
-end
