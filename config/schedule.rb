5b80d2fb0da0d9bdaca52e40accbb3a1298b39aa
Oleg <gafrom@gmail.com>
2018-07-23 22:37:17 +0300

    Speed up time a bit

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,6 @@ every 30.minutes do
   rake 'sync:latest supplier=wb'
 end
 
-every 1.day, at: '7:36 pm' do # server time, currently set to UTC
+every 1.day, at: '7:39 pm' do # server time, currently set to UTC
   rake 'sync:all supplier=wb'
 end
