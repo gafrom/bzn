53c1032e88f089376a944b499aa30fb714a6b080
Oleg <gafrom@gmail.com>
2018-07-24 09:45:22 +0300

    Bring schedule back to normal

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,6 @@ every 30.minutes do
   rake 'sync:latest supplier=wb'
 end
 
-every 1.day, at: '7:39 pm' do # server time, currently set to UTC
+every 1.day, at: '10:01 am' do # server time, currently set to UTC
   rake 'sync:all supplier=wb'
 end
