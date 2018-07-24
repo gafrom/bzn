6a15b8a2caa30c82c6de6b9516468e43f3572c37
Oleg <gafrom@gmail.com>
2018-07-24 21:49:25 +0300

    Add count of available products

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class Admin::DashboardController < AdminController
   def home
     @products_count = Product.where(supplier_id: 12).count
+    @available_products_count = Product.available.where(supplier_id: 12).count
     @latest_updated_product = Product.where(supplier_id: 12).order(updated_at: :desc).first
   end
 end
