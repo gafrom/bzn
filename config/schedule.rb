a9e8f9ad590374bc86dc8b905db2e59bcb5cbfba
Oleg <gafrom@gmail.com>
2018-07-24 20:53:59 +0300

    Change schedule temporarily

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,6 @@ every 30.minutes do
   rake 'sync:latest supplier=wb'
 end
 
-every 1.day, at: '10:01 am' do # server time, currently set to UTC
+every 1.day, at: '5:56 pm' do # server time, currently set to UTC
   rake 'sync:all supplier=wb'
 end
