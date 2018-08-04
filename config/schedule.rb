017859fdc13027bc0d953da56c6b15fb039b5d50
Oleg <gafrom@gmail.com>
2018-08-04 11:13:37 +0300

    Change schedule to free up sunday to avoid clogging network with parallel tasks

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,15 +1,15 @@
 # server time, currently set to UTC
 
-every '7,37 0-9,11-23 * * *' do
+every '7,37 0-9,11-23 * 1-6 *' do
   # trick to set other env variables:
   # rake 'export:xlsx:wb', environment: 'production RAILS_LOG_LEVEL=1'
   rake 'sync:latest supplier=wb export:xlsx:wb'
 end
 
-every 1.day, at: '10:01 am' do
+every '1 10 * 1-6 *' do
   rake 'sync:all supplier=wb export:xlsx:wb'
 end
 
-every :friday, at: '00:08' do
+every :sunday, at: '08:08' do
   rake 'sync:orders_counts supplier=wb export:xlsx:wb'
 end
