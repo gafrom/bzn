d2db2be44153a868dd421c6d1993c09a57ddc28b
Oleg <gafrom@gmail.com>
2018-08-05 09:03:05 +0300

    Fix typo in the schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,12 +1,12 @@
 # server time, currently set to UTC
 
-every '7,37 0-9,11-23 * 1-6 *' do
+every '7,37 0-9,11-23 * * 1-6' do
   # trick to set other env variables:
   # rake 'export:xlsx:wb', environment: 'production RAILS_LOG_LEVEL=1'
   rake 'sync:latest supplier=wb export:xlsx:wb'
 end
 
-every '1 10 * 1-6 *' do
+every '1 10 * * 1-6' do
   rake 'sync:all supplier=wb export:xlsx:wb'
 end
 
