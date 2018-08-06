00107a575b45e423c2b458b923310241ae0a52ab
Oleg <gafrom@gmail.com>
2018-08-06 13:59:19 +0300

    Change schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,12 +1,22 @@
 # server time, currently set to UTC
 
-every '7,37 0-9,11-23 * * 1-6' do
+every '7,37 0-21,23 * * 1-6' do
   # trick to set other env variables:
   # rake 'export:xlsx:wb', environment: 'production RAILS_LOG_LEVEL=1'
   rake 'sync:latest supplier=wb export:xlsx:wb'
 end
 
-every '1 10 * * 1-6' do
+every '7,37 0-5 * * 0' do
+  # trick to set other env variables:
+  # rake 'export:xlsx:wb', environment: 'production RAILS_LOG_LEVEL=1'
+  rake 'sync:latest supplier=wb export:xlsx:wb'
+end
+
+every '2 22 * * 1-6' do
+  rake 'sync:all supplier=wb export:xlsx:wb'
+end
+
+every :sunday, at: '06:06' do
   rake 'sync:all supplier=wb export:xlsx:wb'
 end
 
