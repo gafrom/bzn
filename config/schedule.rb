3aba3c0baf5d0b00842f16948e6c5b225f9ce153
Oleg <gafrom@gmail.com>
2018-11-17 20:48:15 +0300

    Switch back to normal schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -20,9 +20,6 @@ every :sunday, at: '06:06' do
   rake 'sync:all supplier=wb export:xlsx:wb'
 end
 
-# every :sunday, at: '08:08' do
-#   rake 'sync:orders_counts supplier=wb export:xlsx:wb'
-# end
-every :friday, at: '07:25' do
+every :sunday, at: '08:08' do
   rake 'sync:orders_counts supplier=wb export:xlsx:wb'
 end
