3bba3a49aeb935fe4d01d7ab2da3a342c8afd1bb
Oleg <gafrom@gmail.com>
2018-12-19 23:27:37 +0300

    Add a link for juice report file

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,8 @@
 module WelcomeHelper
+  def maybe_add_size(size)
+    size = File.size? Export.path_to_file('juice.xlsx')
+    return if size.blank?
+
+    " (#{number_to_human_size size})"
+  end
 end
