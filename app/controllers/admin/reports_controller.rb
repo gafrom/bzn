ae13f6924d779bb1517b07e057fa4ca16b8f6943
Oleg <gafrom@gmail.com>
2018-12-21 23:39:38 +0300

    Add form

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,17 +1,21 @@
 class Admin::ReportsController < ApplicationController
-  before_action :set_dates
+  before_action :set_dates, only: :create
 
-  def daily
+  def show; end
+
+  def create
     DailyReportJob.perform_later *@dates.map(&:to_s)
 
     human_dates = @dates.map { |date| I18n.l(date) }
 
-    redirect_to admin_root_path, notice: "Set for processing report: #{human_dates}"
+    redirect_to admin_root_path, notice: "Началось создание отчёта с #{human_dates.join(' по ')}"
   end
 
   private
 
   def set_dates
     @dates = [params[:start_at], params[:end_at]].map { |date_str| Date.parse(date_str) }
+  rescue ArgumentError
+    redirect_to admin_root_path, alert: 'Неверный формат дат'
   end
 end
--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -2,7 +2,7 @@ module WelcomeHelper
   def file_description(filname)
     path = Export.path_to_file(filname)
     size = File.size? path
-    return if size.blank?
+    return 'Файл не готов' if size.blank?
 
     "#{number_to_human_size size}, #{I18n.l(File.mtime(path), format: :succinct)}"
   end
