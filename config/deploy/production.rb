ff6f00b01da14036c3d566bd032f9d604b511846
Oleg <gafrom@gmail.com>
2019-03-26 23:47:49 +0300

    Use another production server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '151.248.118.98', user: 'bobby', roles: %w{app db web}
+server '5.63.152.125', user: 'amara', roles: %w{app db web}
 set :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 # set :branch, 'develop'
 set :rails_env, 'production'
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,25 +1,25 @@
 # server time, currently set to UTC
 
-every '7,37 0-21,23 * * 1-6' do
-  # trick to set other env variables:
-  # rake 'export:xlsx:wb', environment: 'production RAILS_LOG_LEVEL=1'
-  rake 'sync:latest supplier=wb export:xlsx:wb'
-end
+# every '7,37 0-21,23 * * 1-6' do
+#   # trick to set other env variables:
+#   # rake 'export:xlsx:wb', environment: 'production RAILS_LOG_LEVEL=1'
+#   rake 'sync:latest supplier=wb export:xlsx:wb'
+# end
 
-every '7,37 0-5 * * 0' do
-  # trick to set other env variables:
-  # rake 'export:xlsx:wb', environment: 'production RAILS_LOG_LEVEL=1'
-  rake 'sync:latest supplier=wb export:xlsx:wb'
-end
+# every '7,37 0-5 * * 0' do
+#   # trick to set other env variables:
+#   # rake 'export:xlsx:wb', environment: 'production RAILS_LOG_LEVEL=1'
+#   rake 'sync:latest supplier=wb export:xlsx:wb'
+# end
 
-every '2 22 * * 1-6' do
-  rake 'sync:all supplier=wb export:xlsx:wb'
-end
+# every '2 22 * * 1-6' do
+#   rake 'sync:all supplier=wb export:xlsx:wb'
+# end
 
-every :sunday, at: '06:06' do
-  rake 'sync:all supplier=wb export:xlsx:wb'
-end
+# every :sunday, at: '06:06' do
+#   rake 'sync:all supplier=wb export:xlsx:wb'
+# end
 
-every :sunday, at: '08:08' do
-  rake 'sync:orders_counts supplier=wb export:xlsx:wb'
-end
+# every :sunday, at: '08:08' do
+#   rake 'sync:orders_counts supplier=wb export:xlsx:wb'
+# end
