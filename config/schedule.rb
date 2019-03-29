7290f749994fc27bc8d95b7145639dc07113b165
Oleg <gafrom@gmail.com>
2019-03-29 23:25:21 +0300

    Change schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,9 @@
 # server time, currently set to UTC
 
-every :friday, at: '12:01' do
-  rake 'sync:all supplier=wb'
-end
-
-# every :sunday, at: '08:08' do
-#   rake 'sync:orders_counts supplier=wb export:xlsx:wb'
+# every :friday, at: '12:01' do
+#   rake 'sync:all supplier=wb'
 # end
+
+every :friday, at: '20:28' do
+  rake 'sync:orders_counts supplier=wb'
+end
