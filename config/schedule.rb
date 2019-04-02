f8238f0e080b791ddb7668e372d00aa49d6d1460
Oleg <gafrom@gmail.com>
2019-04-02 23:51:25 +0300

    Put on hold all background jobs for a while

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,6 +4,6 @@
 #   rake 'sync:all supplier=wb'
 # end
 
-every :friday, at: '20:28' do
-  rake 'sync:orders_counts supplier=wb'
-end
+# every :friday, at: '20:28' do
+#   rake 'sync:orders_counts supplier=wb'
+# end
