df3368fcc86f3bde497101282a4a38ff6188323a
Oleg <gafrom@gmail.com>
2019-10-21 22:22:03 +0100

    Update schedule: move day x to Tuesday

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,25 +1,33 @@
 # server time, currently set to UTC
 
-every '7,37 0-21,23 * * 1-6' do
+every '7,37 0-21,23 * * 0-1,3-6' do
   # trick to set other env variables:
   # rake 'export:xlsx:wb', environment: 'production RAILS_LOG_LEVEL=1'
   rake 'sync:latest supplier=wb export:xlsx:wb'
 end
 
-every '7,37 0-5 * * 0' do
+every '2 22 * * 0-1,3-6' do
+  rake 'sync:all supplier=wb export:xlsx:wb'
+end
+
+every '7,37 0-5 * * 2' do
   # trick to set other env variables:
   # rake 'export:xlsx:wb', environment: 'production RAILS_LOG_LEVEL=1'
   rake 'sync:latest supplier=wb export:xlsx:wb'
 end
 
-every '2 22 * * 1-6' do
+every :tuesday, at: '06:06' do
   rake 'sync:all supplier=wb export:xlsx:wb'
 end
 
-every :sunday, at: '06:06' do
-  rake 'sync:all supplier=wb export:xlsx:wb'
-end
-
-every :sunday, at: '08:08' do
+every :tuesday, at: '08:08' do
   rake 'sync:orders_counts supplier=wb export:xlsx:wb'
 end
+
+# 0 - Sunday
+# 1 - Monday
+# 2 - Tuesday
+# 3 - Wednesday
+# 4 - Thursday
+# 5 - Friday
+# 6 - Saturday
