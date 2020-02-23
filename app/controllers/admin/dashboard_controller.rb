fc0e8ed27902f19c3e1248f01c7cd032bbc3da3a
Oleg <gafrom@gmail.com>
2020-02-23 17:11:48 +0000

    Display stats on recenly released experiment

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,9 +2,11 @@ class Admin::DashboardController < AdminController
   TASKS_LIMIT = 15
 
   def home
-    @products_count = Product.where(supplier_id: 12).count
-    @available_products_count = Product.available.where(supplier_id: 12).count
+    @products_count         = Product.where(supplier_id: 12).count
+    @narrow_available_count = Product.available.where(supplier_id: 12, category_id: 3).count
+    @wide_available_count   = Product.available.where(supplier_id: 12).count
     @latest_updated_product = Product.where(supplier_id: 12).order(updated_at: :desc).first
+
     @tasks = DailyReportTask.all.limit(TASKS_LIMIT).order(id: :desc)
   end
 end
