c7b1d1a6ddf06d21365bdd3dc4f72416e5d6cf97
Oleg <gafrom@gmail.com>
2020-04-18 12:25:16 +0100

    Change grace params for puma

--- config/eye.rb
+++ config/eye.rb
@@ -8,7 +8,6 @@ Eye.application 'bzn' do
   working_dir ROOT_PATH
 
   trigger :flapping, times: 10, within: 1.minute, retry_in: 10.minutes
-  check :cpu, every: 10.seconds, below: 100, times: 3
 
   env 'RAILS_ENV' => 'production'
 
@@ -19,41 +18,40 @@ Eye.application 'bzn' do
 
     start_command "bundle exec puma -C config/puma.rb"
     stop_signals [:QUIT, 5.seconds, :KILL]
-    restart_command 'kill -QUIT {PID}'
+    start_grace 20.seconds
+    restart_grace 25.seconds
 
-    restart_grace 10.seconds
-
-    check :cpu, every: 30, below: 70, times: 3
-    check :memory, every: 1.minute, below: 400.megabytes, times: [3, 5]
+    check :cpu, every: 30.seconds, below: 70, times: 3
+    check :memory, every: 1.minute, below: 300.megabytes, times: [3, 5]
   end
 
   process :sidekiq do
-    pid_file "tmp/pids/sidekiq.pid"
-
     daemonize true
-    start_command "bundle exec sidekiq -c 1 -q default"
-
+    pid_file "tmp/pids/sidekiq.pid"
     stdall "#{ROOT_PATH}/log/sidekiq.log"
+
+    start_command "bundle exec sidekiq -c 1 -q default"
 
     start_grace 30.seconds
     restart_grace 50.seconds
     stop_signals [:TSTP, 5.seconds, :TERM, 12.seconds]
 
-    check :memory, every: 30, below: 150.megabytes, times: 3
+    check :cpu, every: 20.seconds, below: 95, times: 9
+    check :memory, every: 1.minute, below: 150.megabytes, times: 3
   end
 
   process :sidekiq_reports do
-    pid_file "tmp/pids/sidekiq_reports.pid"
-
     daemonize true
-    start_command "bundle exec sidekiq -c 1 -q reports"
-
+    pid_file "tmp/pids/sidekiq_reports.pid"
     stdall "#{ROOT_PATH}/log/sidekiq.log"
+
+    start_command "bundle exec sidekiq -c 1 -q reports"
 
     start_grace 30.seconds
     restart_grace 45.seconds
     stop_signals [:TSTP, 5.seconds, :TERM, 12.seconds]
 
-    check :memory, every: 30, below: 300.megabytes, times: 3
+    check :cpu, every: 20.seconds, below: 95, times: 9
+    check :memory, every: 1.minute, below: 300.megabytes, times: 3
   end
 end
