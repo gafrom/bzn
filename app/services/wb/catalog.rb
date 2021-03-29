986e203c60cc603f6c215ee1831c6be496e00855
Oleg <gafrom@gmail.com>
2021-03-29 21:56:06 +0100

    Reduce storage to 1 month

--- app/services/wb/catalog.rb
+++ app/services/wb/catalog.rb
@@ -431,12 +431,12 @@ module Wb
     end
 
     def delete_old_facts
-      query = DailyFact.where 'created_at < ?', 8.months.ago
+      query = DailyFact.where 'created_at < ?', 1.months.ago
       @deleted_daily_facts_count = query.delete_all
     end
 
     def delete_old_hourly_facts
-      query = HourlyFact.where 'created_at < ?', 8.months.ago
+      query = HourlyFact.where 'created_at < ?', 1.months.ago
       @deleted_hourly_facts_count = query.delete_all
     end
 
